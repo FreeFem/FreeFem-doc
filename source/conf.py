f06462e9180f71939b8fa4cf8f8c5f5f78f77ad2
Simon Garnotel <simon.garnotel@gmail.com>
2019-03-25 15:51:33 +0100

    add sitemap

--- source/conf.py
+++ source/conf.py
@@ -23,6 +23,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.githubpages',
     'sphinxcontrib.inlinesyntaxhighlight',
+    'sphinx_sitemap',
     'subfig'
 ]
 
@@ -64,6 +65,7 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_baseurl = 'https://doc.freefem.org/'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
